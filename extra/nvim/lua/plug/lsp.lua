a264694fcea1d12939f758a004e66848526a9a66
Adam Perkowski <adas1per@protonmail.com>
2025-09-26 01:23:49 +0200

    umm

--- extra/nvim/lua/plug/lsp.lua
+++ extra/nvim/lua/plug/lsp.lua
@@ -1,59 +1,66 @@
-vim.diagnostic.config {
-  signs = false,
-  virtual_text = {
-    virt_text_pos = 'right_align',
-    suffix = ' ',
-  },
-  update_in_insert = true
-}
+vim.diagnostic.config({
+	signs = false,
+	virtual_text = {
+		virt_text_pos = "right_align",
+		suffix = " ",
+	},
+	update_in_insert = true,
+})
 
-local lspconfig = require('lspconfig')
-local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local lspconfig = require("lspconfig")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-lspconfig.rust_analyzer.setup {
-  capabilities = capabilities
-}
+lspconfig.rust_analyzer.setup({
+	capabilities = capabilities,
+})
 
-lspconfig.clangd.setup {
-  capabilities = capabilities
-}
+lspconfig.clangd.setup({
+	capabilities = capabilities,
+})
 
-lspconfig.lua_ls.setup {
-  capabilities = capabilities,
+lspconfig.lua_ls.setup({
+	capabilities = capabilities,
 
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
-        return
-      end
-    end
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+				path ~= vim.fn.stdpath("config")
+				and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
+			then
+				return
+			end
+		end
 
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        version = 'LuaJIT'
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-          "${3rd}/luv/library",
-          "${3rd}/love2d/library",
-        }
-      }
-    })
-  end,
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				version = "LuaJIT",
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+					"${3rd}/luv/library",
+					"${3rd}/love2d/library",
+				},
+			},
+		})
+	end,
 
-  settings = {
-    Lua = {}
-  }
-}
+	settings = {
+		Lua = {},
+	},
+})
 
-lspconfig.jsonls.setup {
-  capabilities = capabilities
-}
+lspconfig.jsonls.setup({
+	capabilities = capabilities,
+})
 
-lspconfig.yamlls.setup {
-  capabilities = capabilities
-}
+lspconfig.yamlls.setup({
+	capabilities = capabilities,
+})
+
+lspconfig.ts_ls.setup({
+	capabilities = capabilities,
+})
