939d7dc70b7251de5f2a3d417e175c032f30bd55
system72 <system72@cock.li>
2025-11-04 13:22:53 +0100

    refact(scripts.ff): use mktemp for kitget output
    Signed-off-by: adam <me@adamperkowski.dev>

--- external/scripts/ff.sh
+++ external/scripts/ff.sh
@@ -7,9 +7,9 @@ if [[ -n $FF_SKIP ]]; then
   exit 0
 fi
 
-kitget_output="/tmp/$(date +%s)"
+kitget_output="$(mktemp --suffix=-kitget.png)"
 
-if [[ -n $FF_NO_KITGET ]] || [[ ! $(kitget --square -o "$kitget_output") ]]; then
+if [[ -n $FF_NO_KITGET ]] || ! kitget --square -o "$kitget_output" &>/dev/null; then
   fastfetch --logo nixos_small --logo-padding-top 3 "$@"
 else
   fastfetch --kitty "$kitget_output" "$@"
