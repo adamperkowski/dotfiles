b5b2b8506e4df2a76c7436686115c48a4b14a395
adam <me@adamperkowski.dev>
2025-12-24 16:48:00 +0100

    base: ssh rate limiting

--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -1,6 +1,14 @@
 {
   networking = {
     networkmanager.enable = true;
-    firewall.enable = true;
+    firewall = {
+      enable = true;
+      extraCommands = ''
+        iptables -I INPUT 1 -p tcp --dport 22 -m conntrack --ctstate NEW \
+          -m recent --name SSH --rsource --update --seconds 120 --hitcount 10 -j DROP
+        iptables -I INPUT 2 -p tcp --dport 22 -m conntrack --ctstate NEW \
+          -m recent --name SSH --rsource --set
+      '';
+    };
   };
 }
