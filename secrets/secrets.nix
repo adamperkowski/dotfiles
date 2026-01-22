62ec7e0d2293ee4b63378c6d1ebef98bd28f732c
koi <me@koi.rip>
2026-01-22 18:42:35 +0100

    chromium: nuke muhehehe

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,6 @@ let
   seber = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAp82e57ij5x+Ei1Tjwxuex0fil5vV9wfEfNYgC5dFQ";
 in
 {
-  "chromium.sh.age".publicKeys = [ koi ];
   "cloudflared.pem.age".publicKeys = [ koi ];
   "jellyfin-rpc.json.age".publicKeys = [ koi ];
 
